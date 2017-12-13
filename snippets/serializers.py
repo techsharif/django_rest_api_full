023520bef61e9df1ffe654b9fc2857b8706731e5
techsharif <sharif.cse.hstu@gmail.com>
2017-12-13 12:19:21 +0600

    4 - before execute permission

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -37,4 +37,15 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        owner = serializers.ReadOnlyField(source='owner.username')
+
+
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
